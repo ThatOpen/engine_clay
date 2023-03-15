010822b6039ba031556f22e95e2b08205c5d0773
QuimMoya <q.moya@hotmail.com>
2023-03-15 12:42:24 +0100

    Add vertices class

--- examples/resources/openbim-clay.js
+++ examples/resources/openbim-clay.js
@@ -1,7 +1,47 @@
-class Vertex {
-    constructor() {
-        console.log("Hello vertex!");
+import * as THREE from 'https://unpkg.com/three@0.135.0/build/three.module.js';
+
+class Vertices {
+    constructor(color = 0x888888, size = 0.1) {
+        this.geometry = new THREE.BufferGeometry();
+        this.vertices = [];
+        this.capacity = 0;
+        this.buffer = new Float32Array(0);
+        this.position = new THREE.BufferAttribute(this.buffer, 3);
+        this.bufferIncrease = 300;
+        this.resetBuffer();
+        const material = new THREE.PointsMaterial({ color, size });
+        this.points = new THREE.Points(this.geometry, material);
+    }
+    add(coordinates) {
+        this.vertices.push(...coordinates);
+        this.regenerate();
+    }
+    setIncreaseBufferValue(value) {
+        this.bufferIncrease = value * 3;
+    }
+    regenerate() {
+        const size = this.vertices.length * 3;
+        if (size >= this.capacity) {
+            this.resetBuffer();
+            for (let i = 0; i < this.vertices.length; i++) {
+                const { x, y, z } = this.vertices[i];
+                this.buffer.set([x, y, z], i * 3);
+            }
+        }
+        else {
+            const indexToAdd = this.vertices.length - 1;
+            const { x, y, z } = this.vertices[indexToAdd];
+            this.buffer.set([x, y, z], indexToAdd * 3);
+        }
+        this.position.count = this.vertices.length;
+    }
+    resetBuffer() {
+        this.capacity += this.bufferIncrease;
+        this.geometry.deleteAttribute("position");
+        this.buffer = new Float32Array(this.capacity);
+        this.position = new THREE.BufferAttribute(this.buffer, 3);
+        this.geometry.setAttribute("position", this.position);
     }
 }
 
-export { Vertex };
+export { Vertices };
--- library/src/primitives/vertex.ts
+++ library/src/primitives/vertex.ts
@@ -1,5 +1,56 @@
-export class Vertex {
-  constructor() {
-    console.log("Hello vertex!");
+import * as THREE from "three";
+
+export class Vertices {
+
+  /** Buffer increment when geometry size is exceeded. */
+  bufferIncrease = 300;
+  
+  /** */
+  points: THREE.Points;
+
+  private geometry = new THREE.BufferGeometry();
+  private vertices: THREE.Vector3[] = [];
+  private capacity = 0;
+  private buffer = new Float32Array(0);
+  private position = new THREE.BufferAttribute(this.buffer, 3);
+
+  constructor(color: THREE.ColorRepresentation = 0x888888, size: number = 0.1) {
+    this.resetBuffer();
+    const material = new THREE.PointsMaterial({ color, size });
+    this.points = new THREE.Points(this.geometry, material);
+  }
+
+  /**
+   * Add new points
+   * @param coordinates Points to add
+   */
+  add(coordinates: THREE.Vector3[]) {
+    this.vertices.push(...coordinates);
+    this.regenerate();
+  }
+
+  private regenerate() {
+    const size = this.vertices.length * 3;
+    if(size >= this.capacity) {
+      this.resetBuffer();
+      return;
+    }
+    const indexToAdd = this.vertices.length - 1;
+    const { x, y, z } = this.vertices[indexToAdd];
+    this.buffer.set([x, y, z], indexToAdd * 3);
+    this.position.count = this.vertices.length;
+  }
+
+  private resetBuffer() {
+    this.capacity += this.bufferIncrease;
+    this.geometry.deleteAttribute("position");
+    this.buffer = new Float32Array(this.capacity);
+    this.position = new THREE.BufferAttribute(this.buffer, 3);
+    this.position.count = this.vertices.length;
+    this.geometry.setAttribute("position", this.position);
+    for (let i = 0; i < this.vertices.length; i++) {
+      const { x, y, z } = this.vertices[i];
+      this.buffer.set([x, y, z], i * 3);
+    }
   }
 }
