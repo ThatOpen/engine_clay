25e26da24c795d9e198c02f37f648eb4ba993f02
QuimMoya <q.moya@hotmail.com>
2023-03-16 15:43:31 +0100

    Each point his own color

--- examples/resources/openbim-clay.js
+++ examples/resources/openbim-clay.js
@@ -4,68 +4,172 @@ class Vertices {
     constructor(size = 0.1) {
         /** Buffer increment when geometry size is exceeded, multiple of 3. */
         this.bufferIncrease = 300;
-        this.geometry = new THREE.BufferGeometry();
-        this.vertices = [];
-        this.colors = [];
-        this.capacity = 0;
-        this.bufferPosition = new Float32Array(0);
-        this.position = new THREE.BufferAttribute(this.bufferPosition, 3);
-        this.bufferColor = new Float32Array(0);
-        this.color = new THREE.BufferAttribute(this.bufferColor, 3);
+        this._geometry = new THREE.BufferGeometry();
+        this._vertices = [];
+        this._colors = [];
+        this._capacity = 0;
+        this._positionBuffer = new THREE.BufferAttribute(new Float32Array(0), 3);
+        this._colorBuffer = new THREE.BufferAttribute(new Float32Array(0), 3);
+        this._selected = new Set();
+        this._selectedColor = new Set();
         this.resetBuffer();
         const material = new THREE.PointsMaterial({
             size,
             vertexColors: true,
         });
-        this.points = new THREE.Points(this.geometry, material);
+        this.points = new THREE.Points(this._geometry, material);
     }
     /**
      * Add new points
      * @param coordinates Points to add
      */
     add(coordinates, colors) {
-        this.vertices.push(...coordinates);
-        this.colors.push(...colors);
+        this._vertices.push(...coordinates);
+        this._colors.push(...colors);
         this.regenerate();
     }
+    /**
+     * Creates a set of selected points
+     * @param active When true we will select, when false we will unselect
+     * @param selection List of point indices to add to the selected set
+     */
+    select(active, selection = []) {
+        if (active) {
+            this.selectIndex(selection);
+            return;
+        }
+        this.unselect(selection);
+    }
+    /**
+     * Apply a displacement vector to the selected points
+     * @param displacement Displacement vector
+     */
+    move(displacement) {
+        const transform = new THREE.Matrix4();
+        transform.setPosition(displacement);
+        this.transform(transform);
+    }
+    /**
+     * Rotate the selected points
+     * @param rotation euler rotation
+     */
+    rotate(rotation) {
+        const transform = new THREE.Matrix4();
+        const { x, y, z } = rotation;
+        transform.makeRotationFromEuler(new THREE.Euler(x, y, z));
+        this.transform(transform);
+    }
+    /**
+     * Scale the selected points
+     * @param scale Scale vector
+     */
+    scale(scale) {
+        const transform = new THREE.Matrix4();
+        transform.scale(scale);
+        this.transform(transform);
+    }
+    selectIndex(selection = []) {
+        if (selection.length === 0) {
+            this.resetAllSelectedColors();
+            this._selected.clear();
+            this._selectedColor.clear();
+            for (let i = 0; i < this._vertices.length; i++) {
+                this.addSelection(i);
+            }
+            return;
+        }
+        for (let i = 0; i < selection.length; i++) {
+            this.addSelection(selection[i]);
+        }
+    }
+    addSelection(index) {
+        this._selected.add(index);
+        this._selectedColor.add(new THREE.Color(this._colorBuffer.getX(index), this._colorBuffer.getY(index), this._colorBuffer.getZ(index)));
+        this._colorBuffer.setX(index, 1);
+        this._colorBuffer.setY(index, 0);
+        this._colorBuffer.setZ(index, 0);
+    }
+    unselect(selection = []) {
+        this.restoreColor(selection);
+        if (selection.length === 0) {
+            this._selected.clear();
+            this._selectedColor.clear();
+            return;
+        }
+        for (let i = 0; i < selection.length; i++) {
+            this._selected.delete(selection[i]);
+        }
+    }
+    restoreColor(selection = []) {
+        if (selection.length === 0) {
+            this.resetAllSelectedColors();
+            return;
+        }
+        this.resetSelectedColors(selection);
+    }
+    resetAllSelectedColors() {
+        const colorArray = Array.from(this._selectedColor);
+        let i = 0;
+        for (const index of this._selected.values()) {
+            this._colorBuffer.setXYZ(index, colorArray[i].r, colorArray[i].g, colorArray[i].b);
+            i++;
+        }
+    }
+    resetSelectedColors(selection) {
+        const selectionArray = Array.from(this._selected);
+        const colorArray = Array.from(this._selectedColor);
+        for (let i = 0; i < selection.length; i++) {
+            const index = selectionArray.indexOf(selection[i]);
+            this._colorBuffer.setXYZ(selection[i], colorArray[index].r, colorArray[index].g, colorArray[index].b);
+        }
+    }
+    transform(transformation) {
+        const vector = new THREE.Vector3();
+        for (const index of this._selected) {
+            const x = this._positionBuffer.getX(index);
+            const y = this._positionBuffer.getY(index);
+            const z = this._positionBuffer.getZ(index);
+            vector.set(x, y, z);
+            vector.applyMatrix4(transformation);
+            this._positionBuffer.setXYZ(index, vector.x, vector.y, vector.z);
+        }
+    }
     regenerate() {
-        const size = this.vertices.length * 3;
-        if (size >= this.capacity) {
+        const size = this._vertices.length * 3;
+        if (size >= this._capacity) {
             this.resetBuffer();
             return;
         }
-        const indexToAdd = this.vertices.length - 1;
-        const { x, y, z } = this.vertices[indexToAdd];
-        this.bufferPosition.set([x, y, z], indexToAdd * 3);
-        const { r, g, b } = this.colors[indexToAdd];
-        this.bufferColor.set([r, g, b], indexToAdd * 3);
-        this.position.count = this.vertices.length;
-        this.color.count = this.vertices.length;
+        const indexToAdd = this._vertices.length - 1;
+        const { x, y, z } = this._vertices[indexToAdd];
+        this._positionBuffer.setXYZ(indexToAdd, x, y, z);
+        const { r, g, b } = this._colors[indexToAdd];
+        this._colorBuffer.setXYZ(indexToAdd, r, g, b);
+        this._positionBuffer.count = this._vertices.length;
+        this._colorBuffer.count = this._vertices.length;
     }
     resetBuffer() {
-        this.capacity += this.bufferIncrease;
+        this._capacity += this.bufferIncrease;
         this.resetAttributePosition();
         this.resetAttributeColor();
-        for (let i = 0; i < this.vertices.length; i++) {
-            const { x, y, z } = this.vertices[i];
-            this.bufferPosition.set([x, y, z], i * 3);
-            const { r, g, b } = this.colors[i];
-            this.bufferColor.set([r, g, b], i * 3);
+        for (let i = 0; i < this._vertices.length; i++) {
+            const { x, y, z } = this._vertices[i];
+            this._positionBuffer.setXYZ(i, x, y, z);
+            const { r, g, b } = this._colors[i];
+            this._colorBuffer.setXYZ(i, r, g, b);
         }
     }
     resetAttributePosition() {
-        this.geometry.deleteAttribute("position");
-        this.bufferPosition = new Float32Array(this.capacity);
-        this.position = new THREE.BufferAttribute(this.bufferPosition, 3);
-        this.position.count = this.vertices.length;
-        this.geometry.setAttribute("position", this.position);
+        this._geometry.deleteAttribute("position");
+        this._positionBuffer = new THREE.BufferAttribute(new Float32Array(this._capacity), 3);
+        this._positionBuffer.count = this._vertices.length;
+        this._geometry.setAttribute("position", this._positionBuffer);
     }
     resetAttributeColor() {
-        this.geometry.deleteAttribute("color");
-        this.bufferColor = new Float32Array(this.capacity);
-        this.color = new THREE.BufferAttribute(this.bufferColor, 3);
-        this.color.count = this.vertices.length;
-        this.geometry.setAttribute("color", this.color);
+        this._geometry.deleteAttribute("color");
+        this._colorBuffer = new THREE.BufferAttribute(new Float32Array(this._capacity), 3);
+        this._colorBuffer.count = this._vertices.length;
+        this._geometry.setAttribute("color", this._colorBuffer);
     }
 }
 
--- library/src/primitives/vertex.ts
+++ library/src/primitives/vertex.ts
@@ -7,14 +7,14 @@ export class Vertices {
   /** THREE points */
   points: THREE.Points;
 
-  private geometry = new THREE.BufferGeometry();
-  private vertices: THREE.Vector3[] = [];
-  private colors: THREE.Color[] = [];
-  private capacity = 0;
-  private bufferPosition = new Float32Array(0);
-  private position = new THREE.BufferAttribute(this.bufferPosition, 3);
-  private bufferColor = new Float32Array(0);
-  private color = new THREE.BufferAttribute(this.bufferColor, 3);
+  private _geometry = new THREE.BufferGeometry();
+  private _vertices: THREE.Vector3[] = [];
+  private _colors: THREE.Color[] = [];
+  private _capacity = 0;
+  private _positionBuffer = new THREE.BufferAttribute(new Float32Array(0), 3);
+  private _colorBuffer = new THREE.BufferAttribute(new Float32Array(0), 3);
+  private _selected = new Set<number>();
+  private _selectedColor = new Set<THREE.Color>();
 
   constructor(size: number = 0.1) {
     this.resetBuffer();
@@ -22,7 +22,7 @@ export class Vertices {
       size,
       vertexColors: true,
     });
-    this.points = new THREE.Points(this.geometry, material);
+    this.points = new THREE.Points(this._geometry, material);
   }
 
   /**
@@ -30,51 +30,188 @@ export class Vertices {
    * @param coordinates Points to add
    */
   add(coordinates: THREE.Vector3[], colors: THREE.Color[]) {
-    this.vertices.push(...coordinates);
-    this.colors.push(...colors);
+    this._vertices.push(...coordinates);
+    this._colors.push(...colors);
     this.regenerate();
   }
 
+  /**
+   * Creates a set of selected points
+   * @param active When true we will select, when false we will unselect
+   * @param selection List of point indices to add to the selected set
+   */
+  select(active: boolean, selection: number[] = []) {
+    if (active) {
+      this.selectIndex(selection);
+      return;
+    }
+    this.unselect(selection);
+  }
+
+  /**
+   * Apply a displacement vector to the selected points
+   * @param displacement Displacement vector
+   */
+  move(displacement: THREE.Vector3) {
+    const transform = new THREE.Matrix4();
+    transform.setPosition(displacement);
+    this.transform(transform);
+  }
+
+  /**
+   * Rotate the selected points
+   * @param rotation euler rotation
+   */
+  rotate(rotation: THREE.Vector3) {
+    const transform = new THREE.Matrix4();
+    const { x, y, z } = rotation;
+    transform.makeRotationFromEuler(new THREE.Euler(x, y, z));
+    this.transform(transform);
+  }
+
+  /**
+   * Scale the selected points
+   * @param scale Scale vector
+   */
+  scale(scale: THREE.Vector3) {
+    const transform = new THREE.Matrix4();
+    transform.scale(scale);
+    this.transform(transform);
+  }
+
+  private selectIndex(selection: number[] = []) {
+    if (selection.length === 0) {
+      this.resetAllSelectedColors();
+      this._selected.clear();
+      this._selectedColor.clear();
+      for (let i = 0; i < this._vertices.length; i++) {
+        this.addSelection(i);
+      }
+      return;
+    }
+    for (let i = 0; i < selection.length; i++) {
+      this.addSelection(selection[i]);
+    }
+  }
+
+  private addSelection(index: number) {
+    this._selected.add(index);
+    this._selectedColor.add(
+      new THREE.Color(
+        this._colorBuffer.getX(index),
+        this._colorBuffer.getY(index),
+        this._colorBuffer.getZ(index)
+      )
+    );
+    this._colorBuffer.setX(index, 1);
+    this._colorBuffer.setY(index, 0);
+    this._colorBuffer.setZ(index, 0);
+  }
+
+  private unselect(selection: number[] = []) {
+    this.restoreColor(selection);
+    if (selection.length === 0) {
+      this._selected.clear();
+      this._selectedColor.clear();
+      return;
+    }
+    for (let i = 0; i < selection.length; i++) {
+      this._selected.delete(selection[i]);
+    }
+  }
+
+  private restoreColor(selection: number[] = []) {
+    if (selection.length === 0) {
+      this.resetAllSelectedColors();
+      return;
+    }
+    this.resetSelectedColors(selection);
+  }
+
+  private resetAllSelectedColors() {
+    const colorArray = Array.from(this._selectedColor);
+    let i = 0;
+    for (const index of this._selected.values()) {
+      this._colorBuffer.setXYZ(
+        index,
+        colorArray[i].r,
+        colorArray[i].g,
+        colorArray[i].b
+      );
+      i++;
+    }
+  }
+
+  private resetSelectedColors(selection: number[]) {
+    const selectionArray = Array.from(this._selected);
+    const colorArray = Array.from(this._selectedColor);
+    for (let i = 0; i < selection.length; i++) {
+      const index = selectionArray.indexOf(selection[i]);
+      this._colorBuffer.setXYZ(
+        selection[i],
+        colorArray[index].r,
+        colorArray[index].g,
+        colorArray[index].b
+      );
+    }
+  }
+
+  private transform(transformation: THREE.Matrix4) {
+    const vector = new THREE.Vector3();
+    for (const index of this._selected) {
+      const x = this._positionBuffer.getX(index);
+      const y = this._positionBuffer.getY(index);
+      const z = this._positionBuffer.getZ(index);
+      vector.set(x, y, z);
+      vector.applyMatrix4(transformation);
+      this._positionBuffer.setXYZ(index, vector.x, vector.y, vector.z);
+    }
+  }
+
   private regenerate() {
-    const size = this.vertices.length * 3;
-    if (size >= this.capacity) {
+    const size = this._vertices.length * 3;
+    if (size >= this._capacity) {
       this.resetBuffer();
       return;
     }
-    const indexToAdd = this.vertices.length - 1;
-    const { x, y, z } = this.vertices[indexToAdd];
-    this.bufferPosition.set([x, y, z], indexToAdd * 3);
-    const { r, g, b } = this.colors[indexToAdd];
-    this.bufferColor.set([r, g, b], indexToAdd * 3);
-    this.position.count = this.vertices.length;
-    this.color.count = this.vertices.length;
+    const indexToAdd = this._vertices.length - 1;
+    const { x, y, z } = this._vertices[indexToAdd];
+    this._positionBuffer.setXYZ(indexToAdd, x, y, z);
+    const { r, g, b } = this._colors[indexToAdd];
+    this._colorBuffer.setXYZ(indexToAdd, r, g, b);
+    this._positionBuffer.count = this._vertices.length;
+    this._colorBuffer.count = this._vertices.length;
   }
 
   private resetBuffer() {
-    this.capacity += this.bufferIncrease;
+    this._capacity += this.bufferIncrease;
     this.resetAttributePosition();
     this.resetAttributeColor();
-    for (let i = 0; i < this.vertices.length; i++) {
-      const { x, y, z } = this.vertices[i];
-      this.bufferPosition.set([x, y, z], i * 3);
-      const { r, g, b } = this.colors[i];
-      this.bufferColor.set([r, g, b], i * 3);
+    for (let i = 0; i < this._vertices.length; i++) {
+      const { x, y, z } = this._vertices[i];
+      this._positionBuffer.setXYZ(i, x, y, z);
+      const { r, g, b } = this._colors[i];
+      this._colorBuffer.setXYZ(i, r, g, b);
     }
   }
 
   private resetAttributePosition() {
-    this.geometry.deleteAttribute("position");
-    this.bufferPosition = new Float32Array(this.capacity);
-    this.position = new THREE.BufferAttribute(this.bufferPosition, 3);
-    this.position.count = this.vertices.length;
-    this.geometry.setAttribute("position", this.position);
+    this._geometry.deleteAttribute("position");
+    this._positionBuffer = new THREE.BufferAttribute(
+      new Float32Array(this._capacity),
+      3
+    );
+    this._positionBuffer.count = this._vertices.length;
+    this._geometry.setAttribute("position", this._positionBuffer);
   }
 
   private resetAttributeColor() {
-    this.geometry.deleteAttribute("color");
-    this.bufferColor = new Float32Array(this.capacity);
-    this.color = new THREE.BufferAttribute(this.bufferColor, 3);
-    this.color.count = this.vertices.length;
-    this.geometry.setAttribute("color", this.color);
+    this._geometry.deleteAttribute("color");
+    this._colorBuffer = new THREE.BufferAttribute(
+      new Float32Array(this._capacity),
+      3
+    );
+    this._colorBuffer.count = this._vertices.length;
+    this._geometry.setAttribute("color", this._colorBuffer);
   }
 }
