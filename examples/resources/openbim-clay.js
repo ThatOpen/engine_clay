498192cde2895c05af60ef7e4cbfbded5fed6fda
QuimMoya <q.moya@hotmail.com>
2023-03-17 10:10:02 +0100

    Add remove

--- examples/resources/openbim-clay.js
+++ examples/resources/openbim-clay.js
@@ -5,13 +5,14 @@ class Vertices {
         /** Buffer increment when geometry size is exceeded, multiple of 3. */
         this.bufferIncrease = 300;
         this.defaultColor = new THREE.Color(0.5, 0.5, 0.5);
-        this._geometry = new THREE.BufferGeometry();
-        this._vertices = [];
-        this._colors = [];
+        this.selectColor = new THREE.Color(1, 0, 0);
         this._capacity = 0;
+        this._geometry = new THREE.BufferGeometry();
         this._positionBuffer = new THREE.BufferAttribute(new Float32Array(0), 3);
         this._colorBuffer = new THREE.BufferAttribute(new Float32Array(0), 3);
         this._selected = new Set();
+        this._geometry.setAttribute("position", this._positionBuffer);
+        this._geometry.setAttribute("color", this._colorBuffer);
         this.resetBuffer();
         const material = new THREE.PointsMaterial({
             size,
@@ -24,11 +25,16 @@ class Vertices {
      * @param coordinates Points to add
      */
     add(coordinates) {
-        this._vertices.push(...coordinates);
-        for (let i = 0; i < this._vertices.length; i++) {
-            this._colors.push(this.defaultColor);
+        this.checkBufferSize(coordinates.length);
+        for (let i = 0; i < coordinates.length; i++) {
+            const indexToAdd = this._positionBuffer.count + i;
+            const { x, y, z } = coordinates[i];
+            this._positionBuffer.setXYZ(indexToAdd, x, y, z);
+            const { r, g, b } = this.defaultColor;
+            this._colorBuffer.setXYZ(indexToAdd, r, g, b);
         }
-        this.regenerate();
+        this._positionBuffer.count += coordinates.length;
+        this._colorBuffer.count += coordinates.length;
     }
     /**
      * Creates a set of selected points
@@ -70,11 +76,33 @@ class Vertices {
         transform.scale(scale);
         this.transform(transform);
     }
+    /**
+     * Removes points from the list
+     */
+    remove() {
+        for (const index of this._selected.values()) {
+            const lastIndex = this._positionBuffer.count - 1;
+            this._positionBuffer.setXYZ(index, this._positionBuffer.getX(lastIndex), this._positionBuffer.getY(lastIndex), this._positionBuffer.getZ(lastIndex));
+            this._colorBuffer.setXYZ(index, this._colorBuffer.getX(lastIndex), this._colorBuffer.getY(lastIndex), this._colorBuffer.getZ(lastIndex));
+            this._positionBuffer.count--;
+        }
+    }
+    checkBufferSize(increase) {
+        const tempPositionArray = this._geometry.getAttribute("position");
+        const size = tempPositionArray.count * 3 + increase * 3;
+        while (size >= this._capacity) {
+            if (this.bufferIncrease < 1) {
+                this.bufferIncrease = 1;
+            }
+            this.resetBuffer();
+            return;
+        }
+    }
     selectIndex(selection = []) {
         if (selection.length === 0) {
             this.resetAllSelectedColors();
             this._selected.clear();
-            for (let i = 0; i < this._vertices.length; i++) {
+            for (let i = 0; i < this._positionBuffer.count; i++) {
                 this.addSelection(i);
             }
             return;
@@ -85,9 +113,9 @@ class Vertices {
     }
     addSelection(index) {
         this._selected.add(index);
-        this._colorBuffer.setX(index, 1);
-        this._colorBuffer.setY(index, 0);
-        this._colorBuffer.setZ(index, 0);
+        this._colorBuffer.setX(index, this.selectColor.r);
+        this._colorBuffer.setY(index, this.selectColor.g);
+        this._colorBuffer.setZ(index, this.selectColor.b);
     }
     unselect(selection = []) {
         this.restoreColor(selection);
@@ -127,41 +155,35 @@ class Vertices {
             this._positionBuffer.setXYZ(index, vector.x, vector.y, vector.z);
         }
     }
-    regenerate() {
-        const size = this._vertices.length * 3;
-        if (size >= this._capacity) {
-            this.resetBuffer();
-            return;
-        }
-        const indexToAdd = this._vertices.length - 1;
-        const { x, y, z } = this._vertices[indexToAdd];
-        this._positionBuffer.setXYZ(indexToAdd, x, y, z);
-        const { r, g, b } = this._colors[indexToAdd];
-        this._colorBuffer.setXYZ(indexToAdd, r, g, b);
-        this._positionBuffer.count = this._vertices.length;
-        this._colorBuffer.count = this._vertices.length;
-    }
     resetBuffer() {
         this._capacity += this.bufferIncrease;
+        const tempPositionArray = this._geometry.getAttribute("position").array;
+        const tempColorArray = this._geometry.getAttribute("color").array;
         this.resetAttributePosition();
         this.resetAttributeColor();
-        for (let i = 0; i < this._vertices.length; i++) {
-            const { x, y, z } = this._vertices[i];
+        for (let i = 0; i < this._positionBuffer.count; i++) {
+            const x = tempPositionArray[i * 3];
+            const y = tempPositionArray[i * 3 + 1];
+            const z = tempPositionArray[i * 3 + 2];
             this._positionBuffer.setXYZ(i, x, y, z);
-            const { r, g, b } = this._colors[i];
+            const r = tempColorArray[i * 3];
+            const g = tempColorArray[i * 3 + 1];
+            const b = tempColorArray[i * 3 + 2];
             this._colorBuffer.setXYZ(i, r, g, b);
         }
     }
     resetAttributePosition() {
+        const count = this._positionBuffer.count;
         this._geometry.deleteAttribute("position");
         this._positionBuffer = new THREE.BufferAttribute(new Float32Array(this._capacity), 3);
-        this._positionBuffer.count = this._vertices.length;
+        this._positionBuffer.count = count;
         this._geometry.setAttribute("position", this._positionBuffer);
     }
     resetAttributeColor() {
+        const count = this._colorBuffer.count;
         this._geometry.deleteAttribute("color");
         this._colorBuffer = new THREE.BufferAttribute(new Float32Array(this._capacity), 3);
-        this._colorBuffer.count = this._vertices.length;
+        this._colorBuffer.count = count;
         this._geometry.setAttribute("color", this._colorBuffer);
     }
 }
