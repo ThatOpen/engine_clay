5ba583694da1aa3062ccbede00462361ce99a0a9
Antonio <antoniogviegas@hotmail.com>
2023-05-26 20:51:01 +0200

    Plane arbitrary moves and rotations

--- resources/openbim-clay.js
+++ resources/openbim-clay.js
@@ -19486,29 +19486,98 @@ class Polygons {
 class Planes {
     constructor(components) {
         this.faces = new Faces();
-        this.transformMode = "TRANSLATE";
         this._enabled = false;
+        this.transformMode = "TRANSLATE";
+        this._lines = new Lines();
         this._selected = null;
+        this._v1 = new THREE.Vector3();
+        this._v2 = new THREE.Vector3();
+        this._v3 = new THREE.Vector3();
+        this._v4 = new THREE.Vector3();
+        this._q = new THREE.Quaternion();
         this._transformActive = false;
         this._previousTransform = new THREE.Matrix4();
         this._newTransform = new THREE.Matrix4();
+        this._angleSelected = false;
+        this._angleStarted = false;
         this.updateTransform = () => {
             const result = this._components.raycaster.castRay([this._helperPlane]);
             if (result === null)
                 return;
-            this._newTransform.setPosition(result.point);
-            const temp = this._newTransform.clone();
-            temp.multiply(this._previousTransform.invert());
+            const { x, y, z } = result.point;
             if (this.transformMode === "TRANSLATE") {
+                this._newTransform.setPosition(result.point);
+                const temp = this._newTransform.clone();
+                temp.multiply(this._previousTransform.invert());
+                this._lines.setPoint(this._helperLine1.end, [x, y, z]);
                 this.faces.transform(temp);
             }
             else if (this.transformMode === "ROTATE") {
-                console.log(temp);
+                if (!this._angleSelected) {
+                    this._lines.setPoint(this._helperLine1.end, [x, y, z]);
+                }
+                else {
+                    this._lines.setPoint(this._helperLine2.end, [x, y, z]);
+                    const first = this._lines.get(this._helperLine1.id);
+                    const second = this._lines.get(this._helperLine2.id);
+                    if (first === null || second === null)
+                        return;
+                    const [[ax, ay, az], [bx, by, bz]] = first;
+                    const [[cx, cy, cz], [dx, dy, dz]] = second;
+                    this._v1.set(ax, ay, az);
+                    this._v2.set(bx, by, bz);
+                    this._v3.set(cx, cy, cz);
+                    this._v4.set(dx, dy, dz);
+                    this._v2.sub(this._v1);
+                    this._v4.sub(this._v3);
+                    let angle = this._v4.angleTo(this._v2);
+                    // Correct angle sign (otherwise it's always the shorter unsigned angle)
+                    this._v3.set(0, 0, 1);
+                    this._v3.applyEuler(this._helperPlane.rotation);
+                    this._v4.cross(this._v2);
+                    const dot = this._v4.dot(this._v3);
+                    if (dot > 0) {
+                        angle *= -1;
+                    }
+                    // Get axis from camera
+                    const axis = new THREE.Vector3();
+                    axis.set(0, 0, 1);
+                    const camera = this._components.camera.get();
+                    axis.applyEuler(camera.rotation);
+                    this._q.setFromAxisAngle(axis, angle);
+                    console.log(angle);
+                    this._newTransform.identity();
+                    const move = new THREE.Matrix4().makeTranslation(this._v1.x, this._v1.y, this._v1.z);
+                    const rotation = new THREE.Matrix4();
+                    rotation.makeRotationFromQuaternion(this._q);
+                    const invMove = move.clone().invert();
+                    this._newTransform.multiply(move);
+                    this._newTransform.multiply(rotation);
+                    this._newTransform.multiply(invMove);
+                    this._newTransform.multiply(this._helperPlane.matrix);
+                    const temp = this._newTransform.clone();
+                    if (!this._angleStarted) {
+                        this._angleStarted = true;
+                        this._previousTransform = this._newTransform.clone();
+                    }
+                    temp.multiply(this._previousTransform.invert());
+                    this.faces.transform(temp);
+                }
             }
             else if (this.transformMode === "SCALE") {
-                console.log(temp);
+                console.log("scale");
             }
             this._previousTransform.copy(this._newTransform);
+        };
+        this.startDrawingAngle = () => {
+            this._angleSelected = true;
+            window.removeEventListener("click", this.startDrawingAngle);
+            window.addEventListener("click", this.finishDrawingAngle);
+        };
+        this.finishDrawingAngle = () => {
+            this._angleSelected = false;
+            window.removeEventListener("click", this.finishDrawingAngle);
+            this.transform(false);
         };
         this.pick = () => {
             if (this._transformActive)
@@ -19528,10 +19597,25 @@ class Planes {
         const material = this.faces.mesh.material;
         material.transparent = true;
         material.opacity = 0.2;
-        const helperMaterial = new THREE.MeshBasicMaterial({ side: 2 });
-        const helperGeometry = new THREE.PlaneGeometry(1000, 1000, 1000);
+        const helperMaterial = new THREE.MeshBasicMaterial({
+            color: 0xeeeeee,
+            transparent: true,
+            opacity: 0.3,
+            side: 2,
+        });
+        const helperGeometry = new THREE.PlaneGeometry(10, 10, 10);
         this._helperPlane = new THREE.Mesh(helperGeometry, helperMaterial);
-        this._helperPlane.visible = false;
+        // this._helperPlane.visible = false;
+        const [a, b, c, d] = this._lines.addPoints([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ]);
+        const [helperLineID] = this._lines.add([a, b]);
+        this._helperLine1 = this._lines.list[helperLineID];
+        const [helperAngleLineID] = this._lines.add([c, d]);
+        this._helperLine2 = this._lines.list[helperAngleLineID];
         const scene = components.scene.get();
         scene.add(this.faces.mesh);
         scene.add(this._helperPlane);
@@ -19562,6 +19646,7 @@ class Planes {
             return;
         this._transformActive = active;
         if (!active) {
+            this.setHelperLineVisible(false);
             this.faces.mesh.geometry.computeBoundingSphere();
             this.faces.mesh.geometry.computeBoundingBox();
             window.removeEventListener("mousemove", this.updateTransform);
@@ -19569,24 +19654,41 @@ class Planes {
         }
         if (this._selected === null)
             return;
+        this.setHelperLineVisible(true);
         const center = this.faces.getCenter(this._selected);
-        const normal = this.faces.getNormal(this._selected);
-        if (center === null || normal === null)
+        if (center === null)
             return;
-        const [x, y, z] = center;
-        const [nx, ny, nz] = normal;
-        this._helperPlane.position.set(x, y, z);
-        this._helperPlane.lookAt(x + nx, y + ny, z + nz);
+        const [cx, cy, cz] = center;
+        this._helperPlane.position.set(cx, cy, cz);
+        const camera = this._components.camera.get();
+        this._helperPlane.rotation.copy(camera.rotation);
         this._helperPlane.updateMatrix();
         this._helperPlane.updateMatrixWorld();
         const result = this._components.raycaster.castRay([this._helperPlane]);
         if (result === null)
             return;
-        console.log(normal);
-        console.log(result.point.y);
         this._previousTransform.setPosition(result.point);
         this._newTransform.setPosition(result.point);
+        const { x, y, z } = result.point;
+        this._lines.setPoint(this._helperLine1.start, [x, y, z]);
+        this._lines.setPoint(this._helperLine1.end, [x, y, z]);
+        this._lines.setPoint(this._helperLine2.start, [x, y, z]);
+        this._lines.setPoint(this._helperLine2.end, [x, y, z]);
+        if (this.transformMode === "ROTATE") {
+            this._angleSelected = false;
+            this._angleStarted = false;
+            window.addEventListener("click", this.startDrawingAngle);
+        }
         window.addEventListener("mousemove", this.updateTransform);
+    }
+    setHelperLineVisible(active) {
+        const scene = this._components.scene.get();
+        if (active) {
+            scene.add(this._lines.mesh, this._lines.vertices.mesh);
+        }
+        else {
+            scene.remove(this._lines.mesh, this._lines.vertices.mesh);
+        }
     }
 }
 
--- src/editors/Planes/index.ts
+++ src/editors/Planes/index.ts
@@ -1,22 +1,38 @@
 import * as THREE from "three";
 import * as OBC from "openbim-components";
-import { Faces } from "../../primitives";
+import { Faces, Lines } from "../../primitives";
 
 export type PlaneTransformMode = "TRANSLATE" | "ROTATE" | "SCALE";
 
 export class Planes {
   faces = new Faces();
-  transformMode: PlaneTransformMode = "TRANSLATE";
+
+  private _enabled = false;
+  private transformMode: PlaneTransformMode = "TRANSLATE";
 
   private _components: OBC.Components;
-  private _enabled = false;
+
+  private _lines = new Lines();
+  private readonly _helperLine1: { id: number; start: number; end: number };
+  private readonly _helperLine2: { id: number; start: number; end: number };
+
+  private readonly _helperPlane: THREE.Mesh;
 
   private _selected: number | null = null;
-  private _helperPlane: THREE.Mesh;
+
+  private _v1 = new THREE.Vector3();
+  private _v2 = new THREE.Vector3();
+  private _v3 = new THREE.Vector3();
+  private _v4 = new THREE.Vector3();
+
+  private _q = new THREE.Quaternion();
 
   private _transformActive = false;
   private _previousTransform = new THREE.Matrix4();
   private _newTransform = new THREE.Matrix4();
+
+  private _angleSelected = false;
+  private _angleStarted = false;
 
   get enabled() {
     return this._enabled;
@@ -37,10 +53,29 @@ export class Planes {
     material.transparent = true;
     material.opacity = 0.2;
 
-    const helperMaterial = new THREE.MeshBasicMaterial({ side: 2 });
-    const helperGeometry = new THREE.PlaneGeometry(1000, 1000, 1000);
+    const helperMaterial = new THREE.MeshBasicMaterial({
+      color: 0xeeeeee,
+      transparent: true,
+      opacity: 0.3,
+      side: 2,
+    });
+
+    const helperGeometry = new THREE.PlaneGeometry(10, 10, 10);
     this._helperPlane = new THREE.Mesh(helperGeometry, helperMaterial);
-    this._helperPlane.visible = false;
+    // this._helperPlane.visible = false;
+
+    const [a, b, c, d] = this._lines.addPoints([
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ]);
+
+    const [helperLineID] = this._lines.add([a, b]);
+    this._helperLine1 = this._lines.list[helperLineID];
+
+    const [helperAngleLineID] = this._lines.add([c, d]);
+    this._helperLine2 = this._lines.list[helperAngleLineID];
 
     const scene = components.scene.get();
     scene.add(this.faces.mesh);
@@ -63,6 +98,7 @@ export class Planes {
     this._transformActive = active;
 
     if (!active) {
+      this.setHelperLineVisible(false);
       this.faces.mesh.geometry.computeBoundingSphere();
       this.faces.mesh.geometry.computeBoundingBox();
       window.removeEventListener("mousemove", this.updateTransform);
@@ -71,24 +107,37 @@ export class Planes {
 
     if (this._selected === null) return;
 
-    const center = this.faces.getCenter(this._selected);
-    const normal = this.faces.getNormal(this._selected);
-    if (center === null || normal === null) return;
+    this.setHelperLineVisible(true);
 
-    const [x, y, z] = center;
-    const [nx, ny, nz] = normal;
-    this._helperPlane.position.set(x, y, z);
-    this._helperPlane.lookAt(x + nx, y + ny, z + nz);
+    const center = this.faces.getCenter(this._selected);
+
+    if (center === null) return;
+
+    const [cx, cy, cz] = center;
+    this._helperPlane.position.set(cx, cy, cz);
+
+    const camera = this._components.camera.get();
+    this._helperPlane.rotation.copy(camera.rotation);
     this._helperPlane.updateMatrix();
     this._helperPlane.updateMatrixWorld();
 
     const result = this._components.raycaster.castRay([this._helperPlane]);
     if (result === null) return;
-    console.log(normal);
-    console.log(result.point.y);
 
     this._previousTransform.setPosition(result.point);
     this._newTransform.setPosition(result.point);
+
+    const { x, y, z } = result.point;
+    this._lines.setPoint(this._helperLine1.start, [x, y, z]);
+    this._lines.setPoint(this._helperLine1.end, [x, y, z]);
+    this._lines.setPoint(this._helperLine2.start, [x, y, z]);
+    this._lines.setPoint(this._helperLine2.end, [x, y, z]);
+
+    if (this.transformMode === "ROTATE") {
+      this._angleSelected = false;
+      this._angleStarted = false;
+      window.addEventListener("click", this.startDrawingAngle);
+    }
 
     window.addEventListener("mousemove", this.updateTransform);
   }
@@ -96,19 +145,103 @@ export class Planes {
   private updateTransform = () => {
     const result = this._components.raycaster.castRay([this._helperPlane]);
     if (result === null) return;
-    this._newTransform.setPosition(result.point);
-    const temp = this._newTransform.clone();
-    temp.multiply(this._previousTransform.invert());
+
+    const { x, y, z } = result.point;
 
     if (this.transformMode === "TRANSLATE") {
+      this._newTransform.setPosition(result.point);
+      const temp = this._newTransform.clone();
+      temp.multiply(this._previousTransform.invert());
+      this._lines.setPoint(this._helperLine1.end, [x, y, z]);
       this.faces.transform(temp);
     } else if (this.transformMode === "ROTATE") {
-      console.log(temp);
+      if (!this._angleSelected) {
+        this._lines.setPoint(this._helperLine1.end, [x, y, z]);
+      } else {
+        this._lines.setPoint(this._helperLine2.end, [x, y, z]);
+
+        const first = this._lines.get(this._helperLine1.id);
+        const second = this._lines.get(this._helperLine2.id);
+        if (first === null || second === null) return;
+
+        const [[ax, ay, az], [bx, by, bz]] = first;
+        const [[cx, cy, cz], [dx, dy, dz]] = second;
+
+        this._v1.set(ax, ay, az);
+        this._v2.set(bx, by, bz);
+        this._v3.set(cx, cy, cz);
+        this._v4.set(dx, dy, dz);
+
+        this._v2.sub(this._v1);
+        this._v4.sub(this._v3);
+
+        let angle = this._v4.angleTo(this._v2);
+
+        // Correct angle sign (otherwise it's always the shorter unsigned angle)
+
+        this._v3.set(0, 0, 1);
+        this._v3.applyEuler(this._helperPlane.rotation);
+        this._v4.cross(this._v2);
+        const dot = this._v4.dot(this._v3);
+        if (dot > 0) {
+          angle *= -1;
+        }
+
+        // Get axis from camera
+
+        const axis = new THREE.Vector3();
+        axis.set(0, 0, 1);
+        const camera = this._components.camera.get();
+        axis.applyEuler(camera.rotation);
+        this._q.setFromAxisAngle(axis, angle);
+
+        console.log(angle);
+
+        this._newTransform.identity();
+
+        const move = new THREE.Matrix4().makeTranslation(
+          this._v1.x,
+          this._v1.y,
+          this._v1.z
+        );
+
+        const rotation = new THREE.Matrix4();
+        rotation.makeRotationFromQuaternion(this._q);
+
+        const invMove = move.clone().invert();
+
+        this._newTransform.multiply(move);
+        this._newTransform.multiply(rotation);
+        this._newTransform.multiply(invMove);
+        this._newTransform.multiply(this._helperPlane.matrix);
+
+        const temp = this._newTransform.clone();
+
+        if (!this._angleStarted) {
+          this._angleStarted = true;
+          this._previousTransform = this._newTransform.clone();
+        }
+
+        temp.multiply(this._previousTransform.invert());
+        this.faces.transform(temp);
+      }
     } else if (this.transformMode === "SCALE") {
-      console.log(temp);
+      console.log("scale");
     }
 
     this._previousTransform.copy(this._newTransform);
+  };
+
+  private startDrawingAngle = () => {
+    this._angleSelected = true;
+    window.removeEventListener("click", this.startDrawingAngle);
+    window.addEventListener("click", this.finishDrawingAngle);
+  };
+
+  private finishDrawingAngle = () => {
+    this._angleSelected = false;
+    window.removeEventListener("click", this.finishDrawingAngle);
+    this.transform(false);
   };
 
   private pick = () => {
@@ -123,4 +256,13 @@ export class Planes {
       this.faces.select(true, [faceID]);
     }
   };
+
+  private setHelperLineVisible(active: boolean) {
+    const scene = this._components.scene.get();
+    if (active) {
+      scene.add(this._lines.mesh, this._lines.vertices.mesh);
+    } else {
+      scene.remove(this._lines.mesh, this._lines.vertices.mesh);
+    }
+  }
 }
