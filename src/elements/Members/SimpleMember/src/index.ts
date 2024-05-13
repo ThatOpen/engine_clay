9d5a080554248f9425a6196078b8f41b627ff741
Cristian Sotomayor <53336800+CristianSotomayorGit@users.noreply.github.com>
2024-05-13 07:32:07 -0700

    feat: enable creation of curtain walls (#18)
    * Enabled creation of plates
    
    * Enabled creation of members and began curtain wall feature
    
    * Enable basic functionality for generation of curtain wall elements
    
    * Fixed bug in curtain wall generation algorithm
    
    * Cleaned algorithm and fixed other bugs
    
    * separated algorithm into instantiation and updates
    
    * fixed bug when instantiating geomtry
    
    ---------
    
    Co-authored-by: cristian sotomayor <csotomayorarch@gmail.com>

---
+++ src/elements/Members/SimpleMember/src/index.ts
@@ -0,0 +1,51 @@
+import { IFC4X3 as IFC } from "web-ifc";
+import { Element } from "../../../Elements";
+import { Model } from "../../../../base";
+import { SimpleMemberType } from "..";
+import { v4 as uuidv4 } from "uuid";
+import { IfcUtils } from "../../../../utils/ifc-utils";
+import { Extrusion, RectangleProfile } from "../../../../geometries";
+import * as THREE from "three";
+
+export class SimpleMember extends Element {
+  attributes: IFC.IfcMember;
+
+  body: Extrusion<RectangleProfile>;
+
+  type: SimpleMemberType;
+
+  width = 0.0635;
+
+  depth = 0.127;
+
+  constructor(model: Model, type: SimpleMemberType) {
+    super(model, type);
+    this.type = type;
+    const location = new THREE.Vector3(0, 0, 1);
+    const placement = IfcUtils.localPlacement(location);
+
+    const profile = new RectangleProfile(model);
+    profile.dimension.x = this.depth;
+    profile.dimension.y = this.width;
+    profile.position = new THREE.Vector3(0, 0, 5);
+    profile.update();
+
+    this.body = new Extrusion(model, profile);
+    const id = this.body.attributes.expressID;
+    this.type.geometries.set(id, this.body);
+    this.geometries.add(id);
+
+    this.attributes = new IFC.IfcPlate(
+      new IFC.IfcGloballyUniqueId(uuidv4()),
+      null,
+      null,
+      null,
+      null,
+      placement,
+      IfcUtils.productDefinitionShape(model, [this.body.attributes]),
+      null,
+      type.memberType
+    );
+    this.model.set(this.attributes);
+  }
+}
